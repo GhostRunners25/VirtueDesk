7415efce12289b393376ea9e916ba6e85c72aef2
IsaacButler25 <isaac.butler@iress.com>
2020-11-17 16:07:50 +0000

    Added IsItem Unit Test

--- src/redux-src/__tests__/ItemManager.test.js
+++ src/redux-src/__tests__/ItemManager.test.js
@@ -1,10 +1,9 @@
-import {GetIcon, ITEM_FILE, ITEM_FOLDER} from "../ItemManager";
+import {GetIcon, IsItem, ITEM_FILE, ITEM_FOLDER} from "../ItemManager";
 import FileIcon from "../../icons/iconfinder_document-text-file-sheet-doc_2931167.svg";
 import FolderIcon from "../../icons/iconfinder_folder-storage-files_2931141.svg";
 
 describe('Item Manager', () => {
   describe('Get Icon', () => {
-
     const testCases = [
       [ITEM_FILE, FileIcon],
       [ITEM_FOLDER, FolderIcon],
@@ -15,11 +14,19 @@ describe('Item Manager', () => {
       const result = GetIcon(itemType, icon);
       expect(result).toEqual(icon);
     });
-
   });
 
   describe('IsItem', () => {
+    const testCases = [
+      [ITEM_FILE, true],
+      [ITEM_FOLDER, true],
+      ["Unknown", false],
+    ]
 
+    it.each(testCases)('IsItem: %s', (itemType, bool) => {
+      const result = IsItem(itemType, bool);
+      expect(result).toEqual(bool);
+    });
   });
 
   describe('GetItemAt', () => {
