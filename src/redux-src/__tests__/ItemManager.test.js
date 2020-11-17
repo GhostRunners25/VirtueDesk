cbb060c5e02a7db5976d6c0d10feeed476a52ab2
IsaacButler25 <isaac.butler@iress.com>
2020-11-17 15:42:30 +0000

    Package Changes

--- src/redux-src/__tests__/ItemManager.test.js
+++ src/redux-src/__tests__/ItemManager.test.js
@@ -2,8 +2,8 @@ import {GetIcon, ITEM_FILE, ITEM_FOLDER} from "../ItemManager";
 import FileIcon from "../../icons/iconfinder_document-text-file-sheet-doc_2931167.svg";
 import FolderIcon from "../../icons/iconfinder_folder-storage-files_2931141.svg";
 
-describe('ItemManager', () => {
-  describe('GetIcon', () => {
+describe('Item Manager', () => {
+  describe('Get Icon', () => {
 
     const testCases = [
       [ITEM_FILE, FileIcon],
