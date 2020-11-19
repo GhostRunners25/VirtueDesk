8b0754f1d7d316a5ae3988167ea622c89181b1b5
Lee Andrews <Lee.Andrews@iress.co.uk>
2020-11-19 16:01:13 +0000

    added unit test for delete-btn.jsx

--- src/components/__tests__/delete-btn.test.jsx
+++ src/components/__tests__/delete-btn.test.jsx
@@ -1,99 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-
 import { TestRenderer } from 'react-redux-test-renderer';
 import { cleanup } from '@testing-library/react';
-
 import DeleteBtn from '../delete-btn';
-import { ItemDeleted } from '../../redux-src/ActionCreators';
-import { reducer } from '../../redux-src/reducer';
+import {ItemDeleted} from "../../redux-src/ActionCreators";
 
 const defaultProps = {
     id: 1,
 };
 
-const defaultState = {
-    lastId: 1,
-    items: [{
-        id: 1,
-        itemType: 'itemType',
-        name: 'name',
-        varAny: 'any',
-        xPos: 0,
-        yPos: 0,
-    }]
-};
-
 const component = new TestRenderer(
     DeleteBtn,
     defaultProps,
-    defaultState,
 );
 
 afterEach(cleanup);
 
-describe('Delete Button Rendering', () => {
-    it('renders without crashing', () => {
-        const div = document.createElement('div');
-        ReactDOM.render(
-            <Provider store={createStore(reducer)}>
-                <DeleteBtn id={1} />
-            </Provider>, div)
-      });
-})
-
-describe('Delete Button Dispatching', () => {
-    it('example 1', () => {
-        const { baseElement, store } = component.renderWithStore(defaultProps, defaultState);
-        baseElement.click(baseElement, { button: 1 });
-        const expectedState = {
-            lastId: 1,
-            items: []
-        };
-        expect(store.getState()).toEqual(expectedState);
-    });
-
-    it('example 2', () => {
-        const { baseElement, store } = component.renderWithStore(defaultProps, defaultState);
-        component.updateStateWithDispatch(defaultState, ItemDeleted);
-        baseElement.click(baseElement, { button: 1 });
-        const expectedState = {
-            lastId: 1,
-            items: []
-        };
-        expect(store.getState()).toEqual(expectedState);
-    });
-
-    it('example 3', () => {
-        const { store } = component.renderWithStore(defaultProps, defaultState);
-        store.dispatch(ItemDeleted, { id: 1 });
-        const expectedState = {
-            lastId: 1,
-            items: []
-        };
-        expect(store.getState()).toEqual(expectedState);
-    });
-
-    it('example 4', () => {
-        component.renderWithStore(defaultProps, defaultState);
-        const action = ItemDeleted(1);
-        const result = reducer(defaultState, action);
-        const expectedState = {
-            lastId: 1,
-            items: []
-        };
-        expect(result).toEqual(expectedState);
-    });
-
-    it('example 5', () => {
-        const { rerender, store } = component.renderWithStore(defaultProps, defaultState);
-        rerender({ ...defaultState, items: []});
-        const expectedState = {
-            lastId: 1,
-            items: []
-        };
-        expect(store.getState()).toEqual(expectedState);
+describe('Delete Button', () => {
+    it('Click', () => {
+        const {store, getByText} = component.renderWithStore(defaultProps);
+        getByText('Delete').click();
+        const action = ItemDeleted(defaultProps.id)
+        expect(store.getActions()).toEqual([action]);
     });
 });
